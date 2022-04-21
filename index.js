629071742aab988b105f37f92d9f302683102d8c
nurhossainfarid <faahsan1516@gmail.com>
2022-04-21 15:47:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const users = [
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com', phone: '0178888888' },
+    { id: 2, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '0178888888' },
+    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '0178888888' },
+    { id: 4, name: 'suchonda', email: 'suchonda@gmail.com', phone: '0178888888' },
+    { id: 5, name: 'srabonti', email: 'srabonti@gmail.com', phone: '0178888888' },
+    { id: 6, name: 'sabila', email: 'sabila@gmail.com', phone: '0178888888' },
+    { id: 7, name: 'sohana', email: 'sohana@gmail.com', phone: '0178888888' },
+]
+
+app.get('/', (req, res) => {
+    res.send('Welcome my over smarty node');
+});
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user);
+})
+
+app.listen(port, () => {
+    console.log('Start my smarty node');
+})
